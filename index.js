107b488ad56a71ab6619d61715bd10c0184e0989
Diana Badas <diana.badas@gmail.com>
2019-07-26 13:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 
   return newObj;
 };
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
